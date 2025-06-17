128f88cfe55687a4519f12780211a5e99f6c9c17
c0a24164f5 <c0a24164f5@edu.teu.ac.jp>
2025-06-17 14:36:23 +0900

    練習３

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -14,6 +14,20 @@ DELTA = { #移動量辞書
     pg.K_RIGHT:(+5,0),
 }
 
+def check_bound(rct:pg.Rect)-> tuple[bool,bool]:
+    """
+    引数：こうかとんrectまたは爆弾rect
+    戻り値：横縦の画面外判定結果
+    画面内True,画面外False    
+    """
+    yoko,tate=True,True
+    if rct.left<0 or WIDTH< rct.right:
+        yoko=False
+    if rct.top<0 or HEIGHT<rct.bottom:
+        tate=False
+    return yoko,tate
+
+
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))
@@ -46,9 +60,17 @@ def main():
                 sum_mv[1] +=mv[1]
                 
         kk_rct.move_ip(sum_mv)
+        if check_bound(kk_rct)!=(True,True):
+            kk_rct.move_ip(-sum_mv[0],-sum_mv[1])
         screen.blit(kk_img, kk_rct)
         screen.blit(bb_img, bb_rct)
         bb_rct.move_ip(vx,vy)
+        yoko,tate=check_bound(bb_rct)
+        if not yoko:
+            vx*=-1
+        if not tate :
+            vy*=-1
+            
         pg.display.update()
         tmr += 1
         clock.tick(50)
