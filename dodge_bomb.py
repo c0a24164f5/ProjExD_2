896cb6d749ca8e2f8e05dafd4ff9f35af78c0207
c0a24164f5 <c0a24164f5@edu.teu.ac.jp>
2025-06-17 14:16:27 +0900

    練習２

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import random
 import pygame as pg
 
 
@@ -22,11 +23,19 @@ def main():
     kk_rct.center = 300, 200
     clock = pg.time.Clock()
     tmr = 0
+    bb_img=pg.Surface((20,20))
+    pg.draw.circle(bb_img,(255,0,0),(10,10),10)
+    bb_rct = bb_img.get_rect()
+    bb_rct.centerx =random.randint(0,WIDTH)
+    bb_rct.centery =random.randint(0,HEIGHT)
+    bb_img.set_colorkey((0,0,0))
+    vx,vy=+50,+50
+    
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: 
                 return
-        screen.blit(bg_img, [0, 0]) 
+        screen.blit(bg_img, [0, 0])
 
         key_lst = pg.key.get_pressed()
         sum_mv = [0, 0]
@@ -38,6 +47,8 @@ def main():
                 
         kk_rct.move_ip(sum_mv)
         screen.blit(kk_img, kk_rct)
+        screen.blit(bb_img, bb_rct)
+        bb_rct.move_ip(vx,vy)
         pg.display.update()
         tmr += 1
         clock.tick(50)
