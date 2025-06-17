c60cd509fdc70dc94a9a639ecc0d352fc949599f
c0a24164f5 <c0a24164f5@edu.teu.ac.jp>
2025-06-17 16:07:52 +0900

    演習問題１ゲームオーバー画面

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import random
+import time
 import pygame as pg
 
 
@@ -27,6 +28,32 @@ def check_bound(rct:pg.Rect)-> tuple[bool,bool]:
         tate=False
     return yoko,tate
 
+def show_game_over(screen: pg.Surface, kk_rct: pg.Rect):
+    """
+    ゲームオーバー時の画面演出を行う関数
+    ブラックアウト＋泣いているこうかとん＋Game Over文字を表示して5秒停止
+    """
+    
+    blackout = pg.Surface((WIDTH, HEIGHT))  
+    blackout.set_alpha(210)  #半透明の黒で画面を覆う
+    pg.draw.rect(blackout, (0, 0, 0,220), (0, 0, WIDTH, HEIGHT))  # 四角を描画
+    screen.blit(blackout, (0, 0))  # 画面に貼り付け
+
+    sad_img = pg.image.load("fig/8.png")  #泣いているこうかとん画像を表示
+    sad_img = pg.transform.rotozoom(sad_img, 0, 0.9)
+
+    font = pg.font.SysFont(None, 100)  #Game Overを表示
+    text = font.render("Game Over", True, (255, 255, 255))  #色
+    text_rct = text.get_rect(center=(WIDTH / 2, HEIGHT / 2))  #画面中央に配置
+    screen.blit(text, text_rct)
+    
+    left_rct = sad_img.get_rect(center=(text_rct.left - 50, text_rct.centery))  #左に配置
+    right_rct = sad_img.get_rect(center=(text_rct.right + 50, text_rct.centery))  #右に配置
+    screen.blit(sad_img, left_rct)
+    screen.blit(sad_img, right_rct)
+
+    pg.display.update() #画面切り替え
+    time.sleep(5) #５秒間表示
 
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
@@ -37,39 +64,39 @@ def main():
     kk_rct.center = 300, 200
     clock = pg.time.Clock()
     tmr = 0
-    bb_img=pg.Surface((20,20))
-    pg.draw.circle(bb_img,(255,0,0),(10,10),10)
+    bb_img=pg.Surface((20,20))  #爆弾の縦横のサイズ
+    pg.draw.circle(bb_img,(255,0,0),(10,10),10)  #爆弾の色
     bb_rct = bb_img.get_rect()
-    bb_rct.centerx =random.randint(0,WIDTH)
-    bb_rct.centery =random.randint(0,HEIGHT)
-    bb_img.set_colorkey((0,0,0))
-    vx,vy=+50,+55
+    bb_rct.centerx =random.randint(0,WIDTH)  #爆弾ランダム座標横
+    bb_rct.centery =random.randint(0,HEIGHT)  #爆弾ランダム座標縦
+    bb_img.set_colorkey((0,0,0))  #余白部分を透明化
+    vx,vy=+5,+5  #爆弾の移動速度
     
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: 
                 return
             
-        if kk_rct.colliderect(bb_rct):
-            print("お前の負け")
+        if kk_rct.colliderect(bb_rct):  #衝突時
+            show_game_over(screen, kk_rct)
             return
         screen.blit(bg_img, [0, 0])
 
         key_lst = pg.key.get_pressed()
         sum_mv = [0, 0]
-        #
-        for key, mv in DELTA.items():
+        
+        for key, mv in DELTA.items():  #こうかとん移動
             if key_lst[key]:
                 sum_mv[0] +=mv[0]
                 sum_mv[1] +=mv[1]
                 
         kk_rct.move_ip(sum_mv)
-        if check_bound(kk_rct)!=(True,True):
+        if check_bound(kk_rct)!=(True,True):  #こうかとんが画面外に行かないように移動量を相殺
             kk_rct.move_ip(-sum_mv[0],-sum_mv[1])
         screen.blit(kk_img, kk_rct)
         screen.blit(bb_img, bb_rct)
         bb_rct.move_ip(vx,vy)
-        yoko,tate=check_bound(bb_rct)
+        yoko,tate=check_bound(bb_rct)  #爆弾が画面端で反射
         if not yoko:
             vx*=-1
         if not tate :
